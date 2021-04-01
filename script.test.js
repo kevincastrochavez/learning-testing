542e21bb41896fec9141e76ecc1c62a635d147ff
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-01 17:05:04 -0600

    created first test for the googleSearch function

--- script.test.js
+++ script.test.js
@@ -1,1 +1,9 @@
-it("this is a test", () => {});
+const googleSearch = require("./script");
+
+dbMock = ["dog.com", " cheese.com", "disney.com", "dogpictures.com"];
+
+it("this is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+
+  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
